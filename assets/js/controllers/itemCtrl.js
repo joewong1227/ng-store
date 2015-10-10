6355a5d30c68f37750266f34c16a77e9ed58c842
wsjwong <wsjwong@outlook.com>
2015-10-10 10:15:24 +0800

    minorChanges

--- assets/js/controllers/itemCtrl.js
+++ assets/js/controllers/itemCtrl.js
@@ -1,40 +1,40 @@
 app.controller("itemCtrl", function($scope) {
-    $scope.items = [
-      {
-        id: 001,
-        img: "img/item/001.jpg",
-        name: "Cool T Shirt",
-        price: 20,
-      },
-      {
-        id: 002,
-        img: "img/item/002.jpg",
-        name: "Nice Dress",
-        price: 30,
-      },
-      {
-        id: 003,
-        img: "img/item/003.jpg",
-        name: "Fun T Shirt",
-        price: 20,
-      },
-      {
-        id: 004,
-        img: "img/item/003.jpg",
-        name: "Cool T Shirt",
-        price: 20,
-      },
-      {
-        id: 005,
-        img: "img/item/002.jpg",
-        name: "Cool T Shirt",
-        price: 20,
-      },
-      {
-        id: 006,
-        img: "img/item/001.jpg",
-        name: "Cool T Shirt",
-        price: 20,
-      },
-    ]
+  $scope.items = [
+    {
+      id: 001,
+      img: "img/item/001.jpg",
+      name: "Cool T Shirt",
+      price: 20,
+    },
+    {
+      id: 002,
+      img: "img/item/002.jpg",
+      name: "Nice Dress",
+      price: 30,
+    },
+    {
+      id: 003,
+      img: "img/item/003.jpg",
+      name: "Fun T Shirt",
+      price: 20,
+    },
+    {
+      id: 004,
+      img: "img/item/003.jpg",
+      name: "Cool T Shirt",
+      price: 20,
+    },
+    {
+      id: 005,
+      img: "img/item/002.jpg",
+      name: "Cool T Shirt",
+      price: 20,
+    },
+    {
+      id: 006,
+      img: "img/item/001.jpg",
+      name: "Cool T Shirt",
+      price: 20,
+    },
+  ]
 });
