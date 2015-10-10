7128549bad46bbedb3ef3d10dba93089aa039279
wsjwong <wsjwong@outlook.com>
2015-10-10 10:48:16 +0800

    Order by sort number

--- assets/js/controllers/itemCtrl.js
+++ assets/js/controllers/itemCtrl.js
@@ -5,36 +5,42 @@ app.controller("itemCtrl", function($scope) {
       img: "img/item/001.jpg",
       name: "Cool T Shirt",
       price: 20,
+      sort: 2,
     },
     {
       id: 002,
       img: "img/item/002.jpg",
       name: "Nice Dress",
       price: 30,
+      sort: 3,
     },
     {
       id: 003,
       img: "img/item/003.jpg",
       name: "Fun T Shirt",
       price: 20,
+      sort: 4,
     },
     {
       id: 004,
       img: "img/item/003.jpg",
       name: "Cool T Shirt",
       price: 20,
+      sort: 1,
     },
     {
       id: 005,
       img: "img/item/002.jpg",
       name: "Cool T Shirt",
       price: 20,
+      sort: 5,
     },
     {
       id: 006,
       img: "img/item/001.jpg",
       name: "Cool T Shirt",
       price: 20,
+      sort: 6,
     },
   ]
 });
