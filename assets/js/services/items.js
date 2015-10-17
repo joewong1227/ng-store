d4de2d0362a3e322c36c3ff72da7050647c242d8
wsjwong <wsjwong@outlook.com>
2015-10-17 16:48:44 +0800

    good routing

--- assets/js/services/items.js
+++ assets/js/services/items.js
@@ -1,7 +1,7 @@
 app.factory('items', ['$http', function($http) { 
   return $http.get('http://localhost:8080/items.json') 
     .success(function(data) { 
-    }) 
+    })
     .error(function(err) {         
     }); 
 }]);
