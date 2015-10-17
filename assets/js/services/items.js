27b82f36ef4624d7c485e8c08afbc0a20506c627
wsjwong <wsjwong@outlook.com>
2015-10-17 16:29:11 +0800

    routeParam works

--- assets/js/services/items.js
+++ assets/js/services/items.js
@@ -1,9 +1,7 @@
 app.factory('items', ['$http', function($http) { 
   return $http.get('http://localhost:8080/items.json') 
     .success(function(data) { 
-      return data;
     }) 
-    .error(function(err) { 
-      return err;              
+    .error(function(err) {         
     }); 
 }]);
