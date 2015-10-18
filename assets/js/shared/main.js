ece6a9b55f24cf80dec7f1c4990e70733127f3fc
wsjwong <wsjwong@outlook.com>
2015-10-18 17:02:58 +0800

    css

--- assets/js/shared/main.js
+++ assets/js/shared/main.js
@@ -11,13 +11,12 @@ simpleCart({
   shippingFlatRate: 20,
   cartStyle: "table",
   cartColumns: [
-    { view: "remove" , text: "x" , label: false },
+    { view: "remove" , text: "Remove" , label: false },
     { attr: "name" , label: "Name" },
-    { attr: "price" , label: "Amount", view: 'currency' },
     { view: "decrement" , label: false , text: "-" },
     { attr: "quantity" , label: "Quantity" },
     { view: "increment" , label: false , text: "+" },
-    { attr: "total" , label: "SubTotal", view: 'currency' },
+    { attr: "total" , label: "Amount", view: 'currency' },
   ]
 });
 
