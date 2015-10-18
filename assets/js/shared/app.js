1b8c297ba0e3e27d86a1b6292cbfe704200a178f
wsjwong <wsjwong@outlook.com>
2015-10-18 11:34:37 +0800

    css

--- assets/js/shared/app.js
+++ assets/js/shared/app.js
@@ -10,6 +10,30 @@ app.config(function ($routeProvider) {
       controller: 'itemCtrl',
       templateUrl: 'views/item.html'
     })
+    .when('/new', {
+      controller: 'itemCtrl',
+      templateUrl: 'views/category/new.html'
+    })
+    .when('/top', {
+      controller: 'itemCtrl',
+      templateUrl: 'views/category/top.html'
+    })
+    .when('/bottom', {
+      controller: 'itemCtrl',
+      templateUrl: 'views/category/bottom.html'
+    })
+    .when('/shoes', {
+      controller: 'itemCtrl',
+      templateUrl: 'views/category/shoes.html'
+    })
+    .when('/accessory', {
+      controller: 'itemCtrl',
+      templateUrl: 'views/category/accessory.html'
+    })
+    .when('/handmade', {
+      controller: 'itemCtrl',
+      templateUrl: 'views/category/handmade.html'
+    })
     .when('/details/:id', {
       controller: 'detailsCtrl',
       templateUrl: 'views/details.html'
