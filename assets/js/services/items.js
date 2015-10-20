c8f11d0081018940f97b2eefe6901b2ec4cc64f7
wsjwong <wsjwong@outlook.com>
2015-10-20 22:44:27 +0800

    desktop view

--- assets/js/services/items.js
+++ assets/js/services/items.js
@@ -1,5 +1,5 @@
 app.factory('items', ['$http', function($http) { 
-  return $http.get('http://localhost:8080/items.json') 
+  return $http.get('http://localhost:8080/items.js') 
     .success(function(data) { 
     })
     .error(function(err) {         
