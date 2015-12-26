4ee3d248355c8d8ff8585d040d80dd8e7f756b7d
Joe W. S. Wong <wsjwong@outlook.com>
2015-12-26 18:19:58 +0800

    bug fixed

--- assets/js/services/items.js
+++ assets/js/services/items.js
@@ -1,5 +1,5 @@
 app.factory('items', ['$http', function($http) { 
-  return $http.get('https://wsjwong.github.io/ngStore/items.json') 
+  return $http.get('https://wsjwong.github.io/ng-store/items.json') 
     .success(function(data) { 
     })
     .error(function(err) {         
